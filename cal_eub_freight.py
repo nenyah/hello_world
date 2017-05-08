a868a8d8e3ca6d231a1aa39d7d67585148ad605a
nenyah <steven@nenyah.com>
2017-05-08 18:57:02 +0800

    format

--- cal_eub_freight.py
+++ cal_eub_freight.py
@@ -7,14 +7,14 @@ import pandas as pd
 import os
 
 def get_ref(country):
-	path = os.path.join(os.path.split(__file__)[0],"epacket.xlsx")
+	path = os.path.join(os.path.split(__file__)[0], "epacket.xlsx")
 	df = pd.read_excel(path)
 	df['country'] = df['country'].str.split(',')
 	is_country = df['country'].map(lambda x: country in x)
 	calcs = df[is_country].T.to_dict()[df[is_country].index[0]]
 	return calcs
 
-def calc_eub(country,weight):
+def calc_eub(country, weight):
 	calcs = get_ref(country)
 	if weight <= calcs['first_weight']:
 		return calcs['r1_price'] * calcs['first_weight'] + calcs['r1_handle']
@@ -24,4 +24,4 @@ def calc_eub(country,weight):
 		return calcs['r2_price'] * weight + calcs['r2_handle']
 
 if __name__ == '__main__':
-	print(calc_eub('sa',40))
+	print(calc_eub('sa', 40))
