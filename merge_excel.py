660167d02b96cda36536074382aa0834580b634a
steven <lucibriel@163.com>
2017-06-16 13:55:43 +0800

    update

--- merge_excel.py
+++ merge_excel.py
@@ -1,67 +1,37 @@
-import openpyxl
+#!/usr/bin/env python3.5
+# -*- coding:utf-8 -*-
+"""
+Function:
+合并目录下的所有excel文件，去重后存入新的excel
+Version:    2016-10-10
+Author:     Steven
+Contact:    lucibriel (at) 163.com
+"""
+ 
 import os
-
-'''
-@author:steven
-date: 2016-05-18
-'''
-
-def get_filenames(path):
-	filenames = []
-	for i in os.walk(path):
-		for filename in i[-1]:
-			full_filename = os.path.join(i[0],filename)
-			filenames.append(full_filename)
-	return filenames
-
-def read_excel(path):
-
-	wb = openpyxl.load_workbook(path)
-	ws = wb.active
-
-	ncols = ws.max_column
-	nrows= ws.max_row
-	titles = []
-	info = {}
-	data = []
-	for i in range(1, ncols+1):
-		titles.append(ws.cell(row=1,column=i).value)
-	# print(titles)
-
-	for row in range(2,nrows+1):
-		temp_list = []
-		for col in range(1, ncols+1):
-			temp_list.append(ws.cell(row=row,column=col).value)
-		data.append(temp_list)
-	# print(data)
-	return data,titles
-
-
-def save_excel(data,titles,path = None):
-	if path is None:
-		path = 'Total.xlsx'
-
-	wb = openpyxl.Workbook()
-	ws = wb.active
-
-	for index, title in enumerate(titles):
-		ws.cell(row=1,column=index+1,value=title)
-	for row,item in enumerate(data):
-		for col,value in enumerate(item):
-			ws.cell(row=row+2,column=col+1,value=value)
-
-	wb.save(path)
-
+import glob
+import pandas as pd
+ 
+ 
+# 攻取目标目录所有
+def get_data(path=r'./*.xls', on=None):
+    """合并数据"""
+    all_data = pd.DataFrame()
+    for f in glob.glob(path):
+        df = pd.read_excel(f)
+#        print(len(df))
+        all_data = all_data.append(df, ignore_index=True)
+    if on:
+        return all_data.drop_duplicates(on)
+    else:
+        return all_data
+ 
+ 
 if __name__ == '__main__':
-	print('Program is running...')
-	path = r'E:\Work\06-Work\00-Todo\Self Calc\Freight Self Calc Download'
-	target_path = r'E:\Work\06-Work\00-Todo\Self Calc\Freight Self Calc Sort'
-	data = []
-	titles = ''
-	for filename in get_filenames(path):
-		titles = read_excel(filename)[-1]
-		for item in read_excel(filename)[0]:
-			if item not in data:
-				data.append(item)
-	save_excel(data,titles,target_path+os.sep+'Total.xlsx')
-	print('Success!')
+    print('Program is running...')
+    path = r'E:\Work\06-Work\00-Todo\Self Calc\Freight Self Calc Download\*'
+    target_path = r'E:\Work\06-Work\00-Todo\Self Calc\Freight Self Calc Sort'
+ 
+    df = get_data(path)
+    df.to_excel(os.path.join(target_path,"Total.xlsx"),index=False)
+    print('Success!')
