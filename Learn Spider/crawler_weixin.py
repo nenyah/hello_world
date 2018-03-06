6ebcb63f53e65cd54f284d9e3cac96def2d7d3ea
steven <steven@nenyah.com>
2018-03-06 16:52:21 +0800

    refactor craler_weixin

--- Learn Spider/crawler_weixin.py
+++ Learn Spider/crawler_weixin.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: steven
 # @Date:   2017-03-04 14:43:55
-# @Last Modified by:   steven
-# @Last Modified time: 2017-09-16 14:07:32
+# @Last Modified by:   Steven
+# @Last Modified time: 2018-03-06 15:30:13
 """
 Function:
 获取指定地址文章
@@ -10,7 +10,6 @@ Version:    2016-09-03
 Author:     Steven
 Contact:    lucibriel (at) 163.com
 """
-from __future__ import unicode_literals
 
 import logging
 import os
@@ -25,6 +24,8 @@ except:
 import pdfkit
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree
+from crawler_tool import request
 
 html_template = """
 <!DOCTYPE html>
@@ -39,6 +40,25 @@ html_template = """
 
 """
 file = r"./pdf/"
+
+log = logging.getLogger()
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s')
+
+file_handler = logging.FileHandler("test.log")
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.formatter = formatter
+
+log.addHandler(file_handler)
+log.addHandler(console_handler)
+
+log.setLevel(logging.INFO)
+
+
+def check_exist(url, path=file):
+    name = url.split('/')[-2] + '.jpg'
+    return not (name in os.listdir(path))
 
 
 class Crawler:
@@ -62,7 +82,7 @@ class Crawler:
         网络请求,返回response对象
         :return:
         """
-        response = requests.get(url, **kwargs)
+        response = request.get(url, 10, **kwargs)
         return response
 
     def parse_menu(self, response):
@@ -83,6 +103,7 @@ class Crawler:
         start = time.time()
         options = {
             'page-size': 'Letter',
+            'image-dpi': 1500,
             'margin-top': '0.75in',
             'margin-right': '0.75in',
             'margin-bottom': '0.75in',
@@ -99,16 +120,20 @@ class Crawler:
         }
         htmls = []
         for index, url in enumerate(self.parse_menu(self.request(self.start_url))):
-            print(url)
+            log.info(url)
             html = self.parse_body(self.request(url))
+
             f_name = ".".join([str(index), "html"])
             with open(file + f_name, 'wb') as f:
                 f.write(html)
             htmls.append(file + f_name)
         try:
-            pdfkit.from_file(htmls, self.name + ".pdf", options=options)
+            config = pdfkit.configuration(
+                wkhtmltopdf=r"D:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+            pdfkit.from_file(htmls, self.name + ".pdf",
+                             options=options, configuration=config)
         except:
-            pass
+            log.error("生成错误", exc_info=True)
         for html in htmls:
             os.remove(html)
         total_time = time.time() - start
@@ -120,18 +145,20 @@ class SaleCrawler(Crawler):
     亚马逊运营
     """
 
+    def download_pic(self, url):
+        name = url.split('/')[-2] + '.jpg'
+        with open(file + name, "wb") as f:
+            f.write(requests.get(url).content)
+
     def parse_menu(self, response):
         """
         解析目录结构,获取所有URL目录列表
         :param response 爬虫返回的response对象
         :return: url生成器
         """
-        soup = BeautifulSoup(response.content, "lxml")
-        menu_tag = soup.find_all("a", href=re.compile(".*mp.weixin.qq.com/.*"))
-        print(menu_tag)
+        tree = etree.HTML(response.text)
+        menu_tag = tree.xpath('//div[@id="js_content"]//a/@href')
         for url in menu_tag:
-            url = url.get("href")
-            # print(url)
             yield url
 
     def parse_body(self, response):
@@ -143,31 +170,41 @@ class SaleCrawler(Crawler):
         try:
             soup = BeautifulSoup(response.content, 'lxml')
             body = soup.find_all(id="img-content")[0]
-            # body.find_all("img")[-2].decompose()
+            body.find_all("img")[-2].decompose()
 
             # 加入标题, 居中显示
             title = body.h2
             title.name = "h1"
             title.wrap(soup.new_tag("center"))
+            profile_container = body.find('div', class_="profile_container")
+
+            del profile_container['style']
 
             html = str(body)
-            p1 = re.compile(r'<script.*"(http.*fmt=.*)";.*script>', re.S)
+            p1 = re.compile(r'<img.*?"(http:\S+)"(?:\s[^>]*)>', re.S)
+            p2 = re.compile(
+                r'<script[^>]*?>[\s\S]*?"(http:\S+)"[\s\S]*?<\/script>', re.S)
 
             def build_img(m):
-                return '<img src="' + m.group(1) + '" />'
-            # body中的img标签的data-src去掉
-            # pattern = "<img(.*?)class="
+                return '<center><img src="' + './' + m.group(1).split('/')[-2] + '.jpg' + '" style="max-width=650px;" /></center>'
+            for href in p2.findall(response.text):
+                if 'mmbiz' in href and check_exist(href):
+                    print(href)
+                    self.download_pic(href)
+            for href in p1.findall(response.text):
+                if 'mmbiz' in href and check_exist(href):
+                    print(href)
+                    self.download_pic(href)
             html = p1.sub(build_img, html)
-            html = re.sub('data-src', 'src', html)
-            html = re.sub("重播", "Day", html)
+            html = p2.sub(build_img, html)
             html = html_template.format(content=html)
             html = html.encode("utf-8")
             return html
         except Exception as e:
-            logging.error("解析错误", exc_info=True)
+            log.error("解析错误", exc_info=True)
 
 
 if __name__ == '__main__':
-    start_url = r"https://mp.weixin.qq.com/s?__biz=MzA4ODY4ODUzNQ==&mid=2663450782&idx=1&sn=30b5160e66c0671df13c8436f6f2ce5f&chksm=8b13fa66bc64737091b196ad4f423adc16609ec4353a5d2a5bc4a42a4236e6e123ec729b9eb3&mpshare=1&scene=23&srcid=0914OIDCGHUY9WcqL2HW6Gxf#rd"
+    start_url = r"https://mp.weixin.qq.com/s?__biz=MzA4ODY4ODUzNQ==&mid=2663450782&idx=1&sn=30b5160e66c0671df13c8436f6f2ce5f"
     crawler = SaleCrawler("顾小北", start_url)
     crawler.run()
--- Learn Spider/dhgate_1.0.py
+++ Learn Spider/dhgate_1.0.py
@@ -17,54 +17,58 @@ from urllib.parse import quote_plus
 import sys
 from numpy import mean
 
+
 def buildUrl(keyword="women shoes", page=1):
-	key_word = quote_plus(keyword)
-	urls = ['http://www.dhgate.com/w/{0}/{1}.html'.format(key_word,str(i)) for i in range(page)]
-	return urls
+    key_word = quote_plus(keyword)
+    urls = (
+        'http://www.dhgate.com/w/{0}/{1}.html'.format(key_word, str(i)) for i in range(page))
+    for url in urls:
+        yield url
 
 
 def getData(url, data=None):
-	data = []
-	web_data = requests.get(url)
-	soup = BeautifulSoup(web_data.text,'lxml')
+    data = []
+    web_data = requests.get(url)
+    soup = BeautifulSoup(web_data.text, 'lxml')
 
-
-	items = soup.find_all("div", "listitem")
-	for item in items:
-		title = item.find("h3").find("a").text # 标题
-		price = item.find("li","price").text #价格
-		m = re.findall(r'(\d+\.*\d+)', price)
-		price = mean(list(map(float, m))) # 计算均价
-		attribute = item.find("ul", "attribute").text
-		min_order = re.findall(r'Min. Order: (\d+)', attribute)[0] #起订量
-		order = re.findall(r'Sold: (\d+)', attribute)
-		order = order[0] if len(order) > 0 else 0 #订单量
-		feedback = item.find("span","reviewnum")
-		feedback = re.findall(r"\d+", feedback.text)[0] if feedback else 0
-		seller = list(item.find("span","seller").stripped_strings)[-1]
-		store_url = item.find("span","seller").find("a")['href']
-		store_feedback = item.find("li","feedback")
-		store_feedback = re.findall(r"\d+\.*\d+", store_feedback.text)[0] if store_feedback else 0
-		info = {
-			'title': title,
-			'price': price,
-			'min_order': min_order,
-			'order': order,
-			'feedback': feedback,
-			'seller': seller,
-			'store_url': store_url,
-			'store_feedback': store_feedback
-		}
-		data.append(info)
-	return data
+    items = soup.find_all("div", "listitem")
+    for item in items:
+        title = item.find("h3").find("a").text  # 标题
+        price = item.find("li", "price").text  # 价格
+        m = re.findall(r'(\d+\.*\d+)', price)
+        price = mean(list(map(float, m)))  # 计算均价
+        attribute = item.find("ul", "attribute").text
+        min_order = re.findall(r'Min. Order: (\d+)', attribute)[0]  # 起订量
+        order = re.findall(r'Sold: (\d+)', attribute)
+        order = order[0] if len(order) > 0 else 0  # 订单量
+        feedback = item.find("span", "reviewnum")
+        feedback = re.findall(r"\d+", feedback.text)[0] if feedback else 0
+        seller = list(item.find("span", "seller").stripped_strings)[-1]
+        store_url = item.find("span", "seller").find("a")['href']
+        store_feedback = item.find("li", "feedback")
+        store_feedback = re.findall(
+            r"\d+\.*\d+", store_feedback.text)[0] if store_feedback else 0
+        info = {
+            'title': title,
+            'price': price,
+            'min_order': min_order,
+            'order': order,
+            'feedback': feedback,
+            'seller': seller,
+            'store_url': store_url,
+            'store_feedback': store_feedback
+        }
+        data.append(info)
+    return data
 
 
 def saveData(data, path='dhgate.csv'):
-	print(data)
-	# with open(path, 'a') as f:
-	# 	f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format())
+    print(data)
+    # with open(path, 'a') as f:
+    # 	f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format())
+
 
 if __name__ == '__main__':
-	urls = buildUrl()
-	data = [getData(url) for url in urls]
-	saveData(data)
+    urls = buildUrl()
+    data = [getData(url) for url in urls]
+    saveData(data)
