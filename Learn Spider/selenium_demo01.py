e94974db4977040470a8c412c186b87442f98970
steven <steven@nenyah.com>
2020-01-22 17:00:12 +0800

    remove fatal file

--- Learn Spider/selenium_demo01.py
+++ Learn Spider/selenium_demo01.py
@@ -25,9 +25,9 @@ def get_shuoshuo(qq):
         driver.switch_to.frame('login_frame')
         driver.find_element_by_id('switcher_plogin').click()
         driver.find_element_by_id('u').clear()#选择用户名框
-        driver.find_element_by_id('u').send_keys('366138476')
+        driver.find_element_by_id('u').send_keys('##')
         driver.find_element_by_id('p').clear()
-        driver.find_element_by_id('p').send_keys('lj870312')
+        driver.find_element_by_id('p').send_keys('##')
         driver.find_element_by_id('login_button').click()
         time.sleep(3)
     driver.implicitly_wait(3)
